57eaf97f10159c1d673d59530429f37c7592a1f0
Antonio Morales <camoralesc52@gmail.com>
2024-09-13 21:23:03 -0600

    To listo to Nais

--- src/main/java/com/thenoob/helloword/Ejercicio_5.java
+++ src/main/java/com/thenoob/helloword/Ejercicio_5.java
@@ -2,6 +2,12 @@ package com.thenoob.helloword;
 
 public class Ejercicio_5 {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        double radio = 4;
+        final double PI = 3.1416;
+
+        double longitud = 2 * PI * radio;
+        double circunferencia = PI * (radio * radio);
+
+        System.out.println("La longitud del círculo con radio de " + radio + " es: " + longitud +" Y su circunferencia es: " + circunferencia);
     }
 }
