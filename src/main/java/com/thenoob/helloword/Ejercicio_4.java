88d552c8f8bb19d65d7c094e0838b37d2e3c935e
Antonio Morales <camoralesc52@gmail.com>
2024-09-13 20:46:16 -0600

    Exercise 4 to nais, to hot

--- src/main/java/com/thenoob/helloword/Ejercicio_4.java
+++ src/main/java/com/thenoob/helloword/Ejercicio_4.java
@@ -1,7 +1,10 @@
 package com.thenoob.helloword;
 
+// Se paso con este, no se de termo asi que queda a su criterio si esta bien :c
 public class Ejercicio_4 {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        int c = 55;
+        int f = (32 + (9 * c/5));
+        System.out.println("El numero " + c + "° en grados Fahrenheit es: "+ f + "°");
     }
 }
