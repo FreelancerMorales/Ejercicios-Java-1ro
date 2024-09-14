ee2b21826702858ccd747a78753e979fcaae6686
Antonio Morales <camoralesc52@gmail.com>
2024-09-13 20:34:53 -0600

    Tarea dos to nais

--- src/main/java/com/thenoob/helloword/Ejercicio_2.java
+++ src/main/java/com/thenoob/helloword/Ejercicio_2.java
@@ -2,6 +2,7 @@ package com.thenoob.helloword;
 
 public class Ejercicio_2 {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        String name = "Juanito";
+        System.out.println("Buenos dias mi estimado " + name);
     }
 }
