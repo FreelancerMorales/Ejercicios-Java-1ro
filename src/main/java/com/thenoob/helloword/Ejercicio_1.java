ad8918180ec6c3841bf3d02d53a14156cbb6a31d
Antonio Morales <camoralesc52@gmail.com>
2024-09-13 20:33:32 -0600

    Primer tarea to nais

--- src/main/java/com/thenoob/helloword/Ejercicio_1.java
+++ src/main/java/com/thenoob/helloword/Ejercicio_1.java
@@ -2,6 +2,8 @@ package com.thenoob.helloword;
 
 public class Ejercicio_1 {
     public static void main(String[] args) {
-        System.out.println("Oe");
+        int num_1 = 54;
+        int num_2 = 27;
+        System.out.println("Aquí vemos el primer Numero "+ num_1+" Y aquí vemos el segundo "+num_2);
     }
 }
