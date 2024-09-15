c507148340b50b45396ed5d2a625be491c3d7717
Antonio Morales <camoralesc52@gmail.com>
2024-09-15 15:03:48 -0600

    Ejercicios 11 y 12 resueltos.

--- src/main/java/com/thenoob/helloword/Ejercicio_12.java
+++ src/main/java/com/thenoob/helloword/Ejercicio_12.java
@@ -1,18 +1,10 @@
 package com.thenoob.helloword;
 
-import java.util.Scanner;
-
-public class Ejercicio_1 {
+public class Ejercicio_12 {
     public static void main(String[] args) {
-
-        System.out.print("Ingrese dato 1: ");
-        Scanner set_1 = new Scanner(System.in);
-        int num_1 = set_1.nextInt();
-
-        System.out.print("Ingrese dato 2: ");
-        Scanner set_2 = new Scanner(System.in);
-        int num_2 = set_2.nextInt();
-
-        System.out.println("Aquí vemos el primer Numero "+ num_1 +" Y aquí vemos el segundo "+ num_2);
+        System.out.println("Despliegue de números impares");
+        for (int i = 1; i <= 100; i+=2) {
+            System.out.println("Numero: " + i);
+        }
     }
 }
