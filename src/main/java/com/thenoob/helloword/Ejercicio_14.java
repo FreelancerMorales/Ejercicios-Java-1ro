c8737f15ef57b4b430635c41084c165a2b4f4471
Antonio Morales <camoralesc52@gmail.com>
2024-09-15 21:19:42 -0600

    Ejercicio 14 completado (Eliminación de Switch, While y Try (Se simplifico con If anidados para un flujo mas estable (Se rompe facilmente al no tener el Try, posible solución volver String)))

--- src/main/java/com/thenoob/helloword/Ejercicio_14.java
+++ src/main/java/com/thenoob/helloword/Ejercicio_14.java
@@ -4,63 +4,58 @@ import java.util.Scanner;
 
 public class Ejercicio_14 {
     public static void main(String[] args) {
-        boolean do_repeat = true, while_repeat = true;
-        int i = 1, respuesta = 0;
+        boolean do_repeat = true;
+        int i = 0, respuesta = 0;
         double promedio = 0, suma = 0;
         int num[] = new int[4];
+
+        Scanner set_data = new Scanner(System.in);
 
         System.out.println("Promedio De Números");
         do {
             if (i < num.length) {
-                Scanner n_num = new Scanner(System.in);
-                System.out.print( i + ".Ingrese un número: ");
-                num[i] = n_num.nextInt();
+                System.out.print((i + 1) + ". Ingrese un número: ");
+                num[i] = set_data.nextInt();
+                i++;
 
+                if (i < num.length) {
+                    System.out.println("");
+                    System.out.println("¿Quiere agregar otro número?");
+                    System.out.println("1. Sí");
+                    System.out.println("2. No");
+
+                    while (true) {
+                        System.out.print("Respuesta: ");
+                        respuesta = set_data.nextInt();
+                        if (respuesta == 1 || respuesta == 2) {
+                            break;
+                        } else {
+                            System.out.println("Respuesta inválida, por favor ingrese 1 o 2.");
+                        }
+                    }
+                } else {
+                    System.err.println("");
+                    System.out.println("Sorry, llegamos al límite de números");
+                    respuesta = 2;
+                }
+            } else {
                 System.out.println("");
-                System.out.println("¿Quiere agregar otro numero?");
-                System.out.println("1. Si");
-                System.out.println("2. No");
-            }else{
-                System.out.println("Sorry, llegamos al limite de números");
+                System.out.println("Sorry, llegamos al límite de números");
                 respuesta = 2;
             }
 
-            while (while_repeat) {
-                try {
-                    if (respuesta == 2) {
-                        while_repeat = false;
-                    }else{
-                        System.out.print("Respuesta: ");
-                        Scanner set_respuesta = new Scanner(System.in);
-                        respuesta = set_respuesta.nextInt();
-                        System.out.println("");
-                            if (respuesta  == 1 || respuesta == 2) {
-                                while_repeat = false;
-                            }else{
-                                System.out.print("Respuesta invalida, por favor ingrese 1 o 2: ");
-                            }
-                    }
-                } catch (Exception e) {
-                    System.out.println("Respuesta invalida, por favor ingrese 1 o 2.");
+            if (respuesta == 2) {
+                for (int j = 0; j < i; j++) {
+                    suma += num[j];
                 }
-            }
+                promedio = suma / i;
 
-            switch (respuesta) {
-                case 1:
-                    i++;
-                break;
+                System.out.println("El promedio de los números ingresados (" + i + ") es: " + String.format("%.2f", promedio));
 
-                case 2:
-                    for (int j = 0; j <= i; j++) {
-                        suma += num[j];
-                    };
-                    promedio = suma / i;
-
-                    System.out.println("El promedio de los números ingresados (" + i + ") es: " + String.format("%.2f", promedio));
-                    // Merezco puntos extras por esto verdad que si?
-                    do_repeat = false;
-                break;
+                do_repeat = false;
             }
         } while (do_repeat);
+        
+        set_data.close();
     }
 }
