df84f05360fc61093f8874f51fcd6372a4ec8bc9
Antonio Morales <camoralesc52@gmail.com>
2024-09-17 19:23:52 -0600

    Reordenamiento completo

---
+++ Tarea No2/Tarea_No2/src/main/java/com/thenoob/tarea_no2/Ejercicio_1.java
@@ -0,0 +1,16 @@
+package com.thenoob.tarea_no2;
+
+import java.util.Scanner;
+
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+        Scanner Num = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        System.out.println("Comparación de Números");
+        
+
+    }
+}
